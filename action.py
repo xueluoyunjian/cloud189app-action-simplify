34a87b776b215b6f907092a9d3c96e00e7da89a4
xueluoyunjian <77815704+xueluoyunjian@users.noreply.github.com>
2023-04-30 22:49:06 +0800

    Update action.py

--- action.py
+++ action.py
@@ -76,8 +76,8 @@ def print_msg(msg: str = "", isFirstLine: bool = False) -> str:
 
 if __name__ == '__main__':
     # 多账号用单个空格 间隔开
-    USERNAME = ''
-    PASSWORD = ''
+    USERNAME = '15551121805'
+    PASSWORD = '2815892394abAB@'
     
     # 启用邮箱发信
     mail_enable = True
